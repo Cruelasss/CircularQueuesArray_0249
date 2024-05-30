9858d731478ea880c99eaf76bf8147509d9eaa81
Cruelas <irula5198@gmail.com>
2024-05-30 14:43:07 +0700

    commit15

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -129,4 +129,15 @@ int main()
                     q.insert();
                     break;
                 }
-              
+                case '2': {
+                    q.remove();
+                    break;
+                }
+               
+    
+
+    
+       
+    
+            
+ 
