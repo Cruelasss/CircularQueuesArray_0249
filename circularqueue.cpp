e96bbf284edb13986152e637f3446994211eb945
Cruelas <irula5198@gmail.com>
2024-05-30 14:39:57 +0700

    commit8

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -63,5 +63,16 @@ void insert()
                 FRONT = FRONT + 1;
         }
     }
+void display()
+    {
+        int FRONT_Position = FRONT;
+        int REAR_Position = REAR;
 
-    
+        //cek apakah antrian kosong
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        
