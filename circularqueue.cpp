10c42ac8885ab40f4d2b04d554aca151ac85cf48
Cruelas <irula5198@gmail.com>
2024-05-30 14:42:49 +0700

    commit14

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -123,8 +123,10 @@ int main()
             cin >> ch;
             cout << endl;
 
-           
-
-        
-            
- 
+            switch (ch)
+            {
+                case '1': {
+                    q.insert();
+                    break;
+                }
+              
